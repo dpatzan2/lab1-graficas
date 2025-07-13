0362f0abcfe5390510d71226170a8ee6a7aa9495
Diego Patzan <diegopatzan24@gmail.com>
2025-07-13 16:50:46 -0600

    adding poligon 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod framebuffer;
+mod polygon;
+
+use framebuffer::Framebuffer;
+use polygon::fill_polygon;
+use raylib::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut framebuffer = Framebuffer::new(800, 600, Color::WHITE);
+    framebuffer.set_background_color(Color::WHITE);
+    framebuffer.clear();
+
+    let poly1 = vec![
+        Vector2::new(165.0, 380.0), Vector2::new(185.0, 360.0), Vector2::new(180.0, 330.0), Vector2::new(207.0, 345.0),
+        Vector2::new(233.0, 330.0), Vector2::new(230.0, 360.0), Vector2::new(250.0, 380.0), Vector2::new(220.0, 385.0),
+        Vector2::new(205.0, 410.0), Vector2::new(193.0, 383.0),
+    ];
+
+    
+
+    framebuffer.set_current_color(Color::RED);
+    fill_polygon(&mut framebuffer, &poly1);
+
 }
