5e0299e653439b08d0e27535d40d7d2df362b6d2
Diego Patzan <diegopatzan24@gmail.com>
2025-07-13 16:56:02 -0600

    adding poligon 4

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,15 @@ fn main() {
         Vector2::new(377.0, 249.0), Vector2::new(411.0, 197.0), Vector2::new(436.0, 249.0),
     ];
 
-    
+    let poly4 = vec![
+        Vector2::new(413.0, 177.0), Vector2::new(448.0, 159.0), Vector2::new(502.0, 88.0), Vector2::new(553.0, 53.0),
+        Vector2::new(535.0, 36.0), Vector2::new(676.0, 37.0), Vector2::new(660.0, 52.0), Vector2::new(750.0, 145.0),
+        Vector2::new(761.0, 179.0), Vector2::new(672.0, 192.0), Vector2::new(659.0, 214.0), Vector2::new(615.0, 214.0),
+        Vector2::new(632.0, 230.0), Vector2::new(580.0, 230.0), Vector2::new(597.0, 215.0), Vector2::new(552.0, 214.0),
+        Vector2::new(517.0, 144.0), Vector2::new(466.0, 180.0),
+    ];
+
+  
 
     framebuffer.set_current_color(Color::RED);
     fill_polygon(&mut framebuffer, &poly1);
@@ -35,8 +43,10 @@ fn main() {
     framebuffer.set_current_color(Color::BLUE);
     fill_polygon(&mut framebuffer, &poly3);
 
-  
+    framebuffer.set_current_color(Color::ORANGE);
+    fill_polygon(&mut framebuffer, &poly4);
 
+    
     framebuffer.render_to_file("out.png");
     framebuffer.render_to_bmp("out.bmp");
     println!("Imagen generada como out.png y out.bmp");
