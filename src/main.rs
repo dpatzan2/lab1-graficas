b386014db92d147a15bee6bd7027f81277dba74d
Diego Patzan <diegopatzan24@gmail.com>
2025-07-13 16:52:24 -0600

    adding poligon 2

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,20 @@ fn main() {
         Vector2::new(205.0, 410.0), Vector2::new(193.0, 383.0),
     ];
 
-    
+    let poly2 = vec![
+        Vector2::new(321.0, 335.0), Vector2::new(288.0, 286.0), Vector2::new(339.0, 251.0), Vector2::new(374.0, 302.0),
+    ];
+
 
     framebuffer.set_current_color(Color::RED);
     fill_polygon(&mut framebuffer, &poly1);
 
+    framebuffer.set_current_color(Color::GREEN);
+    fill_polygon(&mut framebuffer, &poly2);
+
+
+
+    framebuffer.render_to_file("out.png");
+    framebuffer.render_to_bmp("out.bmp");
+    println!("Imagen generada como out.png y out.bmp");
 }
